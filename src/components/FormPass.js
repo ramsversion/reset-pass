103b4f76dcf4855d1df6f39a8bc2082cd7ab60dd
Rodrigo Morales <correorodrigomorales@gmail.com>
2022-09-29 20:06:47 -0300

    spinner

--- src/components/FormPass.js
+++ src/components/FormPass.js
@@ -1,35 +1,69 @@
-import * as React from 'react';
-import  requestPassword  from '../requests/server'
+import { useState } from 'react';
+import requestPassword from '../requests/server';
+import { useForm } from "react-hook-form";
 
-
+const [ending, setEnding] = useState(false);
 const FormPass = () => {
-  requestPassword('1');
-  const handleSubmit = (event) => {
+  const [loading, setLoading] = useState(false);
+  
+  const [endMessage, setEndMessage] = useState('');
+
+
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setLoading(true);
     const email = event.target.elements.email.value;
-    const password = event.target.elements.password.value;
-
-    alert(email + ' ' + password);
+    const status = await requestPassword(email);
+    setLoading(false);
+    setEnding(status.data.Success);
+    setEndMessage(status.data.Message);
+    console.log(status.data.Success);
   };
 
+
+  // function onSubmit(data) {
+  //   //const email = data.target.elements.email.value;
+  //   // return promise that resolves after 2 seconds
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 2000);
+  //   });
+  // }
+
   return (
-    <form id="register-form" className="form" method="post" onSubmit={handleSubmit} >
+    
+      {ending 
+        ? <form id="register-form" className="form" method="post" onSubmit={handleSubmit} >
+          <div className="form-group ">
+            <div className="input-group">
+              <span className="input-group-addon"><i className="glyphicon glyphicon-envelope color-blue"></i></span>
+              <input id="email" name="email" placeholder="email address" className="form-control" type="email" />
+            </div>
+          </div>
 
-      <div className="form-group ">
-        <div className="input-group">
-          <span className="input-group-addon"><i className="glyphicon glyphicon-envelope color-blue"></i></span>
-          <input id="email" name="email" placeholder="email address" className="form-control" type="email" />
-        </div>
-      </div>
+          <div className="form-group">
+            {/* <input name="recover-submit" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" /> */}
+            <button disabled={loading} className="btn btn-lg btn-primary btn-block">
+              Reset Password
+              <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+              {loading && <span className="spinner-border text-primary"></span>}
+            </button>
+          </div>
 
-      <div className="form-group">
-        <input name="recover-submit" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" />
-      </div>
 
-      <input type="hidden" className="hide" name="token" id="token" value="" />
-    </form>
-  );
+
+
+          <input type="hidden" className="hide" name="token" id="token" value="" />
+        </form>
+
+      :
+
+        <h3>{endMessage}</h3>
+  )
 };
 
 export { FormPass };
--- src/requests/server.js
+++ src/requests/server.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
-const API_URL = process.env.REACT_APP_THEME_API_URL
+const API_URL = process.env.REACT_APP_THEME_API_URL + 'auth/create-url-reset-password';
 
-export default  function requestPassword(email) {
-    return axios.post(API_URL, {
-        email,
-    })
+export default async function requestPassword(email: any) {
+    const res = await axios.get(API_URL, { params: { email: email } });
+    return res;
 }
 
